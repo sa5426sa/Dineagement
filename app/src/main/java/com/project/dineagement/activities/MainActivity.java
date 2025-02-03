7da7d938895d10b5a6bdcc1d53f038ddb0202f32
Shaked <sa5426@bs.amalnet.k12.il>
2025-02-03 09:31:39 +0200

    Initial commit

---
+++ app/src/main/java/com/project/dineagement/activities/MainActivity.java
@@ -0,0 +1,16 @@
+package com.project.dineagement.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.project.dineagement.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
